a173872da8d7d9b44a81fad9bf0ce90145693ac3
John Wellbrock <wellbrock23john@gmail.com>
2020-11-18 17:39:27 -0700

    config update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	PORT: process.env.PORT || 3000,
-	API_ENDPOINT: "http://localhost:8000",
-	TOKEN_KEY: "gentle-token",
+	API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000",
+	TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || "gentle-token",
 };
