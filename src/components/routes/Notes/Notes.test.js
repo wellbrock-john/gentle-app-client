83503d455c74dbd33d8175bc6b7d975cf0e592f5
John Wellbrock <wellbrock23john@gmail.com>
2020-11-19 21:05:24 -0700

    fixed treasure and notes tests

--- src/components/routes/Notes/Notes.test.js
+++ src/components/routes/Notes/Notes.test.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Notes from "./Notes";
+import Context from "../../../Context";
+import { BrowserRouter as Router } from "react-router-dom";
 
 it("renders without crashing", () => {
 	const div = document.createElement("div");
-	ReactDOM.render(<Notes />, div);
+	ReactDOM.render(
+		<Context.Provider value={{ notes: [] }}>
+			<Router>
+				<Notes />
+			</Router>
+		</Context.Provider>,
+		div
+	);
 	ReactDOM.unmountComponentAtNode(div); //cleanup
 });
--- src/components/routes/Treasure/Treasure.test.js
+++ src/components/routes/Treasure/Treasure.test.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Treasure from "./Treasure";
-
+import Context from "../../../Context";
 import { BrowserRouter as Router } from "react-router-dom";
 
 it("renders without crashing", () => {
 	const div = document.createElement("div");
 	ReactDOM.render(
-		<Router>
-			<Treasure />
-		</Router>,
+		<Context.Provider value={{ positivestatements: [] }}>
+			<Router>
+				<Treasure />
+			</Router>
+		</Context.Provider>,
 		div
 	);
 	ReactDOM.unmountComponentAtNode(div); //cleanup
